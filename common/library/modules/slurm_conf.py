6ad57833b2368a0e808fe3ce75132c1ab72be084
Jagadeesh N V <jagadeesh_n_v@dell.com>
2026-01-23 18:34:00 +0530

    Added CSV types slurm.conf

--- common/library/modules/slurm_conf.py
+++ common/library/modules/slurm_conf.py
@@ -60,10 +60,10 @@ options:
         default: slurm
         choices: ['slurm', 'cgroup', 'gres', 'mpi', 'slurmdbd']
 author:
-    - Jagadeesh N V (jagadeesh.n.v@dell.com)
+    - Jagadeesh N V (@jagadeeshnv)
 notes:
-    - Requires Python 3.7+ for ordered dict behavior.
     - Array-type parameters (NodeName, PartitionName, SlurmctldHost, etc.) are handled specially.
+    - Module is not case sensitive for conf keys
 '''
 
 EXAMPLES = r'''
@@ -140,12 +140,16 @@ ini_lines:
     sample: ["ClusterName=mycluster", "SlurmctldTimeout=120"]
 '''
 
+# TODO: Support for validation of S_P_<data> types
+# TODO: Validation for choices for each type
+# TODO: Choices types for each type
+# TODO: Merge of sub options
+# TODO: Hostlist expressions, split and merge computations
+
 from collections import OrderedDict
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.input_validation.common_utils.slurm_conf_utils import SlurmParserEnum, all_confs
 import os
-
-# NOTE: depends on python3.7+ where dict order is maintained
 
 
 def read_dict2ini(conf_dict):
@@ -176,7 +180,7 @@ def parse_slurm_conf(file_path, module):
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"{file_path} not found.")
 
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
             # handles any comment after the data
             line = line.split('#')[0].strip()
@@ -188,24 +192,25 @@ def parse_slurm_conf(file_path, module):
             items = line.split()
             tmp_dict = OrderedDict()
             for item in items:
-                # module.warn(f"Item: {item}")
                 # Split only on the first '=' to allow '=' inside the value
                 key, value = item.split('=', 1)
                 tmp_dict[key.strip()] = value.strip()
             skey = list(tmp_dict.keys())[0]
             if skey not in current_conf:
-                raise Exception(f"Invalid key while parsing {file_path}: {skey}")
-            # if current_conf[skey] == SlurmParserEnum.S_P_ARRAY or len(tmp_dict) > 1:
+                raise ValueError(f"Invalid key while parsing {file_path}: {skey}")
             if current_conf[skey] == SlurmParserEnum.S_P_ARRAY:
                 # TODO hostlist expressions and multiple DEFAULT entries handling
-                # if len(tmp_dict) == 1:
-                #     first_key = list(tmp_dict.keys())[0]
-                #     first_value = list(tmp_dict.values())[0]
-                #     slurm_dict[first_key] = list(
-                #         slurm_dict.get(first_key, [])) + [first_value]
-                # else:
+                module.warn(f"{skey} is S_P_ARRAY")
                 slurm_dict[list(tmp_dict.keys())[0]] = list(
                     slurm_dict.get(list(tmp_dict.keys())[0], [])) + [tmp_dict]
+            elif current_conf[skey] == SlurmParserEnum.S_P_CSV:
+                module.warn(f"{skey} is S_P_CSV")
+                existing_values = [v.strip() for v in slurm_dict.get(skey, "").split(',') if v.strip()]
+                new_values = [v.strip() for v in tmp_dict[skey].split(',') if v.strip()]
+                slurm_dict[skey] = ",".join(list(dict.fromkeys(existing_values + new_values)))
+            elif current_conf[skey] == SlurmParserEnum.S_P_LIST:
+                module.warn(f"{skey} is S_P_LIST")
+                slurm_dict[skey] = list(slurm_dict.get(skey, [])) + list(tmp_dict.values())
             else:
                 # TODO handle csv values, currently no definite data type for csv values
                 slurm_dict.update(tmp_dict)
@@ -214,29 +219,33 @@ def parse_slurm_conf(file_path, module):
 
 
 def slurm_conf_dict_merge(conf_dict_list, module):
+    # TODO: Case insensitivity
     merged_dict = OrderedDict()
+    conf_name = module.params['conf_name']
+    current_conf = all_confs.get(conf_name)
     for conf_dict in conf_dict_list:
         for ky, vl in conf_dict.items():
-            if isinstance(vl, list):
+            if current_conf[ky] == SlurmParserEnum.S_P_ARRAY:
                 for item in vl:
                     if isinstance(item, dict):
-                        # module.warn(f"DICT Key: {ky}, Value: {vl}")
                         existing_dict = merged_dict.get(ky, {})
                         inner_dict = existing_dict.get(item.get(ky), {})
                         inner_dict.update(item)
                         # TODO Partition node combiner logic
                         existing_dict[item.get(ky)] = inner_dict
                         merged_dict[ky] = existing_dict
-                    else:
-                        # module.warn(f"LIST Key: {ky}, Value: {vl}")
-                        existing_list = merged_dict.get(ky, [])
-                        # module.warn(f"Existing list: {existing_list}")
-                        # module.warn(f"Item: {item}")
-                        if item not in existing_list:
-                            # existing_list.append(item)
-                            existing_list.update(item)
-                        # module.warn(f"Updated list: {existing_list}")
-                        merged_dict[ky] = existing_list
+            elif current_conf[ky] == SlurmParserEnum.S_P_LIST:
+                existing_list = merged_dict.get(ky, [])
+                if isinstance(vl, list):
+                    new_items = vl
+                else:
+                    new_items = [vl]
+                merged_dict[ky] = list(dict.fromkeys(existing_list + new_items))
+            elif current_conf[ky] == SlurmParserEnum.S_P_CSV:
+                existing_values = [v.strip() for v in merged_dict.get(ky, "").split(',') if v.strip()]
+                new_values = [v.strip() for v in vl.split(',') if v.strip()]
+                merged_dict[ky] = ",".join(list(dict.fromkeys(existing_values + new_values)))
+                # module.exit_json(changed=True, existing_values=existing_values, new_values=new_values,ky=ky,vl=vl)
             else:
                 merged_dict[ky] = vl
     # flatten the dict
@@ -280,18 +289,16 @@ def run_module():
                     conf_dict_list.append(conf_source)
                 elif isinstance(conf_source, str):
                     if not os.path.exists(conf_source):
-                        raise Exception(f"File {conf_source} does not exist")
+                        raise FileNotFoundError(f"File {conf_source} does not exist")
                     s_dict = parse_slurm_conf(conf_source, module)
-                    # module.warn(f"Conf dict: {s_dict}")
                     conf_dict_list.append(s_dict)
-                    # module.warn("After append")
                 else:
-                    raise Exception(f"Invalid type for conf_source: {type(conf_source)}")
-            # module.exit_json(changed=False, conf_dict=conf_dict_list)
+                    raise TypeError(f"Invalid type for conf_source: {type(conf_source)}")
+            # module.exit_json(changed=True, conf_dict_list=conf_dict_list)
             merged_dict = slurm_conf_dict_merge(conf_dict_list, module)
             result['conf_dict'] = merged_dict
             result['ini_lines'] = read_dict2ini(merged_dict)
-    except Exception as e:
+    except (FileNotFoundError, ValueError, TypeError) as e:
         result['failed'] = True
         result['msg'] = str(e)
         module.fail_json(msg=str(e))
